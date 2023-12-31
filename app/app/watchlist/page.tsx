673f90ea5fe77f8c5daec8911a132bfbd3d5083b
m-naim <mehenaouin@gmail.com>
2023-12-31 20:19:16 +0100

    unused imports

--- app/app/watchlist/page.tsx
+++ app/app/watchlist/page.tsx
@@ -1,40 +1,6 @@
 'use client'
 
 import React from 'react'
-import {
-  ColumnDef,
-  ColumnFiltersState,
-  SortingState,
-  VisibilityState,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
-} from '@tanstack/react-table'
-import { ArrowUpDown, ChevronDown, MoreHorizontal } from 'lucide-react'
-
-import { Button } from '@/components/molecules/ui/button'
-import { Checkbox } from '@/components/molecules/ui/checkbox'
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/molecules/ui/dropdown-menu'
-import { Input } from '@/components/molecules/ui/input'
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/molecules/ui/table'
 
 export default function Projects() {
   return (
