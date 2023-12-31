eeef4c2a070124496579aa49bbee630fa1dd2139
m-naim <mehenaouin@gmail.com>
2023-12-31 21:03:03 +0100

    🔢 analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 
+import { Analytics } from '@vercel/analytics/react';
 import { Space_Grotesk } from 'next/font/google'
-import { Analytics, AnalyticsConfig } from 'pliny/analytics'
+import { Analytics as An, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
@@ -75,7 +76,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
-          <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
+          <Analytics />
+          <An analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
             <div className="flex h-screen flex-col justify-between font-sans">
               <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
