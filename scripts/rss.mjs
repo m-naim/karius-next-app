18055c02d20420c339c8ca8c4655bf62cea8f5ad
m-naim <mehenaouin@gmail.com>
2025-09-11 16:02:16 +0200

    fix: tmp remove postbuild

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -28,7 +28,7 @@ const generateRss = (config, posts, page = 'feed.xml') => `
       <language>${config.language}</language>
       <managingEditor>${config.email} (${config.author})</managingEditor>
       <webMaster>${config.email} (${config.author})</webMaster>
-      <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
+      <lastBuildDate>${new Date(posts[0]?.date).toUTCString()}</lastBuildDate>
       <atom:link href="${config.siteUrl}/${page}" rel="self" type="application/rss+xml"/>
       ${posts.map((post) => generateRssItem(config, post)).join('')}
     </channel>
