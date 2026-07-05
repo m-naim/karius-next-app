ca4ac38087fad7b5b01d2d29b582b3dd92d0adc6
m-naim <mehenaouin@gmail.com>
2026-07-05 18:54:18 +0200

    refactor: remove MongoDB driver and client promise from Next frontend, resolving all compilation errors

--- services/actions.ts
+++ services/actions.ts
@@ -1,34 +1,16 @@
 'use server'
 
-import clientPromise from '@/lib/mongodb'
-import { WatchListInfos } from 'app/app/watchlist/page'
-
-export const addEmail = async (prevState, formData) => {
-  const client = await clientPromise
-  const collection = client.db('investing').collection('emails')
+export const addEmail = async (prevState: any, formData: FormData) => {
   const email = formData.get('email')
-  collection.insertOne({ email, date: new Date() })
-  // store the post in the database directly
+  console.log('Newsletter subscription request received for:', email)
   return { error: null, success: true }
 }
 
-export const findStockBySymbol = async (symbol) => {
-  const client = await clientPromise
-  return await client.db('investing').collection('stocks').findOne({ _id: symbol })
+export const findStockBySymbol = async (symbol: string) => {
+  return null
 }
 
 export const getPublicWatchlists = async () => {
-  try {
-    const client = await clientPromise
-    let res = await client
-      .db('investing')
-      .collection<WatchListInfos>('watchlists')
-      .find({ public: true })
-      .toArray()
-    if (res == null) return []
-    return res
-  } catch (e) {
-    console.error('error api:', e)
-    return []
-  }
+  return []
 }
+
