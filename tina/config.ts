5d661f12e2bac016d625d6f4cbfe33af8563a125
m-naim <mehenaouin@gmail.com>
2026-08-09 16:43:03 +0200

    fix: reorder environment variable checks for branch configuration in tina config

--- tina/config.ts
+++ tina/config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'tinacms'
 
 const branch =
-  process.env.HEAD ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
   process.env.NEXT_PUBLIC_TINA_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
   'main'
 
 export default defineConfig({
